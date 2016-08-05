e4e4c3292e06ddc0a386636c352d41c348ec51b6
富山涼平 <tommy@fnana.jp>
2016-08-05 11:18:03 +0900

    hoge

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import todoApp from './reducers'
-import App from './components/App'
-
-let store = createStore(todoApp)
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+module.exports = {
+	entry: './index.js',
+	output: {
+		path: './',
+		filename: 'bundle.js',
+		publicPath: ''
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	}
+}
