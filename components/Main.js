d43a3a858cec887bac969966482bde6b521bdfab
富山涼平 <tommy@fnana.jp>
2016-08-11 09:55:45 +0900

    CSSModules

--- components/Main.js
+++ components/Main.js
@@ -5,12 +5,13 @@ import styles from '../css/modules.css';
 
 const Main = ({isOpen}) => {
 	return (
-		<div className={styles.open}>
+		<div className={(() => {
+					return isOpen? styles.open : styles.close
+				})()}>
 		ほげ
 		</div>
 	)
 }
-
 Main.PropTypes = {
   isOpen: PropTypes.bool.isRequired,
 }
